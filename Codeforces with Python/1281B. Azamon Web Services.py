df6b281a8060dcd9b2902b4a02c0ea6bad32ed73
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-07-15 23:17:50 +0600

    Round 1281

--- Codeforces with Python/1281B. Azamon Web Services.py
+++ Codeforces with Python/1281B. Azamon Web Services.py
@@ -1,46 +1,26 @@
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 for _ in range(int(input())):
     s,t=map(str,input().split())
-        
-    temp1=[];temp2=[];res=[]
-    ls=len(s);lt=len(t)
-    
-    for i in range(ls):
-        temp1.append((s[i],i))
-        temp2.append((s[i],i))
-        res.append(s[i])
-        
-    temp2.sort()
-    
-    flag1=[0,10000]
-    flag2=[0,10000]
-    for i in range(ls):
-        if temp2[i][0]<temp1[i][0] and temp2[i][1]>temp1[i][1]:
-            flag1=list(temp1[i]);flag2=list(temp2[i])
-            break
 
-    pres=[]
-    for i in range(ls):
-        if i==flag1[1]:
-            pres.append(flag2[0])
-        elif i==flag2[1]:
-            pres.append(flag1[0])
-        else:
-            pres.append(res[i])
+    arr = []
 
+    for i,char in enumerate(s):
+        temp = letters.index(char)
+        arr.append((char,i))
+    arr.sort()
+    
     flag=''
-    for i in range(min(ls,lt)):
-        if pres[i]!=t[i]:
-            if pres[i]<t[i]:
-                print(''.join(pres))
+    for i,char in enumerate(s):
+        for j in range(len(s)):
+            if arr[j][0]>char:
+                break
+
+            if arr[j][0]<char and arr[j][1]>i:
+                new = arr[j][1]
+                cur = i
                 flag='done'
                 break
-            else:
-                print('---')
-                flag='done'
-                break
-            
-    if flag=='':
-        if ls<lt:
-            print(''.join(pres))
-        else:
-            print('---')
+        if flag=='done':
+            break
+        
