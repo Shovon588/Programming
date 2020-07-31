1a0d8057069ea3b983abc87fe2fbc631aa34722a
Shovon588 <mainulislam588@gmail.com>
2020-07-31 20:35:19 +0600

    Round 1388

--- Codeforces with Python/1388B. Captain Flint and a Long Voyage.py
+++ Codeforces with Python/1388B. Captain Flint and a Long Voyage.py
@@ -4,24 +4,10 @@ for I in range(int(input())):
     nine = needed//4
     rem = needed%4
 
-    result = nine*'9'
+    for i in range(nine):
+        print(9,end='')
+ 
+    for i in range(n-nine):
+        print(8,end='')
 
-    '''
-    if rem>0:
-        maxi = -1
-        res = -1
-        for i in range(10):
-            temp = '000' + bin(i)[2:]
-            temp = temp[-4:]
-            
-            if int(temp[:rem])>maxi:
-                maxi = int(temp[:rem])
-                res = i
-            
-        result+=str(res)
-    '''
-        
-    for i in range(n-len(result)):
-        result+='8'
-
-    print(result)
+    print()
